ad65fece18cf83a5356394846f55bf029bd5297a
brs98 <southwick.brandon21gmail.com>
2022-02-12 19:51:26 -0700

    Change colorscheme and clean up

--- vim/.vim/lua/brs98/plugins.lua
+++ vim/.vim/lua/brs98/plugins.lua
@@ -45,7 +45,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-  use "ellisonleao/gruvbox.nvim" -- Colorscheme
+  use "LunarVim/darkplus.nvim"
   use "kyazdani42/nvim-web-devicons"
   use "kyazdani42/nvim-tree.lua"
   use "akinsho/bufferline.nvim"
