e5c592375ef8e8cecf7882c814671111d3e910f5
brs98 <southwick.brandon21gmail.com>
2022-05-25 23:35:11 -0600

    Add vscode config for neovim and minor styling to nvim-tree

--- vim/.vim/lua/brs98/nvim-tree.lua
+++ vim/.vim/lua/brs98/nvim-tree.lua
@@ -45,13 +45,13 @@ nvim_tree.setup {
   sort_by = "name",
   update_cwd = false,
   view = {
-    width = 30,
+    width = 50,
     height = 30,
     hide_root_folder = false,
     side = "left",
     preserve_window_proportions = false,
     number = false,
-    relativenumber = false,
+    relativenumber = true,
     signcolumn = "yes",
     mappings = {
       custom_only = false,
