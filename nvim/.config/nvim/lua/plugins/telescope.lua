60e998e64e98820f189a7221d65fb617a0e80681
brs98 <southwick.brandon21@gmail.com>
2023-05-11 14:43:11 -0600

    Add keybindings for tmux and neovim. Add WebsiteProjects to sessionizer

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,5 +6,23 @@ return {
       require("telescope.builtin").resume,
       { silent = true, noremap = true, desc = "Resume previous search" },
     },
+    {
+      "<leader><space>",
+      function()
+        require("telescope.builtin").find_files({ cwd = false })
+      end,
+      silent = true,
+      noremap = true,
+      desc = "Find files (root dir)",
+    },
+    {
+      "<leader>/",
+      function()
+        require("telescope.builtin").live_grep({ cwd = false })
+      end,
+      silent = true,
+      noremap = true,
+      desc = "Grep (root dir)",
+    },
   },
 }
