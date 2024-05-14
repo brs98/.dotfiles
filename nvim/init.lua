5d30e0d50074b8821a803d7f1a8de4bf70904030
brs98 <southwick.brandon21@gmail.com>
2024-05-14 02:33:54 -0600

    add alpha-nvim for dashboard and add to treesitter ensure_installed

--- nvim/init.lua
+++ nvim/init.lua
@@ -299,6 +299,44 @@ require("lazy").setup({
 	},
 
 	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.dashboard").config)
+			local dashboard = require("alpha.themes.dashboard")
+			-- ascii art for "DevXPerience"
+			local logo = [[
+    ██████╗ ███████╗██╗   ██╗██╗  ██╗██████╗ ███████╗██████╗ ██╗███████╗███╗   ██╗ ██████╗███████╗
+    ██╔══██╗██╔════╝██║   ██║╚██╗██╔╝██╔══██╗██╔════╝██╔══██╗██║██╔════╝████╗  ██║██╔════╝██╔════╝
+    ██║  ██║█████╗  ██║   ██║ ╚███╔╝ ██████╔╝█████╗  ██████╔╝██║█████╗  ██╔██╗ ██║██║     █████╗  
+    ██║  ██║██╔══╝  ╚██╗ ██╔╝ ██╔██╗ ██╔═══╝ ██╔══╝  ██╔══██╗██║██╔══╝  ██║╚██╗██║██║     ██╔══╝  
+    ██████╔╝███████╗ ╚████╔╝ ██╔╝ ██╗██║     ███████╗██║  ██║██║███████╗██║ ╚████║╚██████╗███████╗
+    ╚═════╝ ╚══════╝  ╚═══╝  ╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝╚══════╝
+  ]]
+
+			dashboard.section.header.val = vim.split(logo, "\n")
+  -- stylua: ignore
+  dashboard.section.buttons.val = {
+    dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
+    dashboard.button("n", " " .. " New file",        "<cmd> ene <BAR> startinsert <cr>"),
+    dashboard.button("r", " " .. " Recent files",    "<cmd> Telescope oldfiles <cr>"),
+    dashboard.button("g", " " .. " Find text",       "<cmd> Telescope live_grep <cr>"),
+    -- dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+    dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
+    dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
+  }
+			for _, button in ipairs(dashboard.section.buttons.val) do
+				button.opts.hl = "AlphaButtons"
+				button.opts.hl_shortcut = "AlphaShortcut"
+			end
+			dashboard.section.header.opts.hl = "AlphaHeader"
+			dashboard.section.buttons.opts.hl = "AlphaButtons"
+			dashboard.section.footer.opts.hl = "AlphaFooter"
+			dashboard.opts.layout[1].val = 8
+		end,
+	},
+
+	{
 		"nvim-tree/nvim-tree.lua",
 		version = "*",
 		lazy = false,
@@ -1109,7 +1147,30 @@ require("lazy").setup({
 
 			---@diagnostic disable-next-line: missing-fields
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
+				ensure_installed = {
+					"bash",
+					"c",
+					"html",
+					"lua",
+					"markdown",
+					"vim",
+					"vimdoc",
+					"css",
+					"csv",
+					"dockerfile",
+					"hoon",
+					"javascript",
+					"json",
+					"kdl",
+					"nix",
+					"prisma",
+					"python",
+					"rust",
+					"sql",
+					"svelte",
+					"typescript",
+					"yaml",
+				},
 				-- Autoinstall languages that are not installed
 				auto_install = true,
 				highlight = { enable = true },
