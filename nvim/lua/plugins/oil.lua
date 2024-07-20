8e186483fd4878096489bd9e15cd17a6a7dec2e1
brs98 <southwick.brandon21@gmail.com>
2024-07-20 16:56:08 -0600

    add oil keymap

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,23 @@
 return {
-	"stevearc/oil.nvim",
-	config = function()
-		require("oil").setup()
-	end,
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				columns = { "icon" },
+				keymaps = {
+					["<C-h>"] = false,
+					["<C-l>"] = false,
+					["<C-k>"] = false,
+					["<C-j>"] = false,
+				},
+				view_options = {
+					show_hidden = true,
+				},
+			})
+
+			-- Open parent directory in floating window
+			vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+		end,
+	},
 }
