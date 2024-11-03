c9a20824a49e79e62edc9de8ebce137690a6fb9d
brs98 <southwick.brandon21@gmail.com>
2024-11-03 14:22:23 -0700

    remove comment

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -9,7 +9,6 @@
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true; # default shell on catalina
-  # programs.fish.enable = true;
 
   # Set Git commit hash for darwin-version.
   system.configurationRevision = self.rev or self.dirtyRev or null;
