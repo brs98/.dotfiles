6399d1212cd034139d7a5b60d0b2cef6e27b850d
brs98 <southwick.brandon21@gmail.com>
2025-02-03 16:55:20 -0700

    add nixos ozone to fix scaling issues on some apps

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -53,11 +53,11 @@
   };
 
   # # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+  services.xserver.enable = true;
   #
   # # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   #
   # # Configure keymap in X11
   # services.xserver.xkb = {
@@ -130,9 +130,14 @@
   # Install firefox.
   programs.firefox.enable = true;
 
-  programs.hyprland = { enable = true; };
+  programs.hyprland = { 
+    enable = true;
+  };
 
   environment.sessionVariables = {
+    # Hint electron apps to use wayland
+    NIXOS_OZONE_WL = "1";
+
     BROWSER = "google-chrome";
   };
 
