797e60e087a24d51be9d1a056e8dadd1c39f31f7
Your Name <your.email@example.com>
2025-08-09 01:52:55 -0600

    update script darwin-rebuild command

--- install-mac.sh
+++ install-mac.sh
@@ -79,4 +79,4 @@ log "Applying nix-darwin configuration..."
 cd "$DOTFILES_DIR"
 
 # Use the default configuration - nix-darwin will handle hostname internally
-sudo -E HOME="$HOME" nix run nix-darwin -- switch --flake ".#default" --impure
+sudo -E HOME="$HOME" darwin-rebuild switch --flake ".#default" --impure
