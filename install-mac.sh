af8660b4932d6b2242f12a1c7e50a67dc6ebafd7
brs98 <southwick.brandon21@gmail.com>
2025-08-27 10:39:34 -0600

    Updates to install-mac.sh

--- install-mac.sh
+++ install-mac.sh
@@ -20,7 +20,7 @@ fi
 
 if ! command -v nix >/dev/null 2>&1; then
   log "Installing Nix..."
-  sh <(curl -L https://nixos.org/nix/install) --daemon
+  sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 else
   log "Nix already installed."
@@ -38,45 +38,8 @@ else
   git -C "$DOTFILES_DIR" pull
 fi
 
-# Configure Git user information
-if [ -z "${GIT_USER_NAME:-}" ] || [ -z "${GIT_USER_EMAIL:-}" ]; then
-  log "Setting up Git configuration..."
-  
-  if [ -z "${GIT_USER_NAME:-}" ]; then
-    prompt "Enter your Git username:"
-    read -r GIT_USER_NAME
-    export GIT_USER_NAME
-  fi
-  
-  if [ -z "${GIT_USER_EMAIL:-}" ]; then
-    prompt "Enter your Git email:"
-    read -r GIT_USER_EMAIL  
-    export GIT_USER_EMAIL
-  fi
-  
-  # Save to shell profile for persistence
-  SHELL_PROFILE=""
-  if [ -n "${ZSH_VERSION:-}" ]; then
-    SHELL_PROFILE="$HOME/.zshrc"
-  elif [ -n "${BASH_VERSION:-}" ]; then
-    SHELL_PROFILE="$HOME/.bashrc"
-  else
-    SHELL_PROFILE="$HOME/.profile"
-  fi
-  
-  log "Adding Git configuration to $SHELL_PROFILE..."
-  {
-    echo ""
-    echo "# Git configuration for dotfiles"
-    echo "export GIT_USER_NAME=\"$GIT_USER_NAME\""
-    echo "export GIT_USER_EMAIL=\"$GIT_USER_EMAIL\""
-  } >> "$SHELL_PROFILE"
-  
-  log "Git configuration saved. These environment variables will be available in new shell sessions."
-fi
-
 log "Applying nix-darwin configuration..."
 cd "$DOTFILES_DIR"
 
 # Use the default configuration - nix-darwin will handle hostname internally
-sudo -E HOME="$HOME" sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run nix-darwin/master#darwin-rebuild -- switch --flake ".#default" --impure
+sudo -E HOME="$HOME" sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run nix-darwin/master#darwin-rebuild -- switch --flake ".#default"
