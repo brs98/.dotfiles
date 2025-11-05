c0391231e55b6922ed08092e7b0688ce91f49ef3
Brandon Southwick <southwick.brandon21@gmail.com>
2025-11-04 20:41:22 -0700

    neovim cleanup

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -5,7 +5,6 @@ return {
 		for k, v in pairs(opts) do
 			leap.opts[k] = v
 		end
-		leap.add_default_mappings()
 		vim.keymap.del({ "x", "o" }, "x")
 		vim.keymap.del({ "x", "o" }, "X")
 		vim.keymap.set({ "n", "v" }, "s", "^", { silent = true, noremap = true })
