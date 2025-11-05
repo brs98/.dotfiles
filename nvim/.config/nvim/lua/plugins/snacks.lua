d03ebb08d4b2fe1447b96bd9212988e0cc222f47
Brandon Southwick <southwick.brandon21@gmail.com>
2025-11-05 01:36:13 -0700

    Enable more snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,5 +3,10 @@ return {
 	opts = {
 		image = {},
 		scroll = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		notifier = { enabled = true },
+		scope = { enabled = true },
+		statuscolumn = { enabled = true },
 	},
 }
