e2c75cc9c905f4d22e8fe8941b74a3bf3ae7626b
Brandon Southwick <southwick.brandon21@gmail.com>
2025-11-10 00:50:04 -0700

    tokyonight nvim default to dark background

--- linux/symlink/omarchy/.config/omarchy/themes/tokyo-night/neovim.lua
+++ linux/symlink/omarchy/.config/omarchy/themes/tokyo-night/neovim.lua
@@ -1,10 +1,11 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme tokyonight-night")
-    end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.o.background = "dark"
+			vim.cmd("colorscheme tokyonight-night")
+		end,
+	},
 }
