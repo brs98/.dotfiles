6b673520c6662ab22b0623f70fa95abfbdd74115
Brandon Southwick <southwick.brandon21@gmail.com>
2025-12-01 12:01:09 -0700

    Add wezterm config

--- shared/stow/nvim/.config/nvim/lua/plugins/theme.lua
+++ shared/stow/nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,11 +1,11 @@
 return {
 	{
-		"folke/tokyonight.nvim",
-		name = "tokyonight",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd("colorscheme tokyonight")
+			vim.cmd("colorscheme catppuccin-mocha")
 		end,
 	},
 }
